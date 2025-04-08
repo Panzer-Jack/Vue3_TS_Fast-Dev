e31e2866cdca6048e3d16ee63344ee841ac87946
Panzer_Jack <shenchang01@bilibili.com>
2025-04-08 19:28:43 +0800

    feat: add test

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,10 +1,10 @@
 import antfu from '@antfu/eslint-config'
 
 export default antfu({
-    unocss: true,
-    formatters: true,
-    pnpm: true,
-},{
+  unocss: true,
+  formatters: true,
+  pnpm: true,
+}, {
   ignores: ['node_modules', '**/node_modules/**', 'dist', '**/dist/**'],
   rules: {
     'ts/no-explicit-any': 'off',
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 import UnoCSS from 'unocss/vite'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
+import { defineConfig } from 'vite'
+import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -45,7 +45,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
